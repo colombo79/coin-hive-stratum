543a4703c0f48d1090a2cd8e4b2aecbf901204af
colombo79 <34789535+colombo79@users.noreply.github.com>
2017-12-24 10:13:27 +0200

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,13 +1,13 @@
 module.exports = {
-  host: "pool.supportxmr.com",
-  port: 3333,
+  host: "xmr.pool.minergate.com",
+  port: 45560,
   pass: "x",
   ssl: false,
   address: null,
-  user: null,
+  user: "oren.sofrin@gmail.com",
   diff: null,
   dynamicPool: false,
-  maxMinersPerConnection: 100,
+  maxMinersPerConnection: 10000,
   donations: [
     {
       address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
